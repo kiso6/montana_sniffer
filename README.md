68faa3a41a1923e8ce05d2ba576eedc20ba9aa69
Kilian S <kiliansoual@outlook.fr>
2023-10-29 17:17:28 +0100

    added features

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # montana_sniffer
  Python homemade sniffer 
+
+
+## Install Montana 
+To install Montana, you only have to clone the following repository
+
+```bash
+git clone https://github.com/kiso6/montana_sniffer.git
+```
+
+## Use Montana 
+###  Generic use  
+Montana is a commande line based sniffer. You can use as follows:
+
+```bash
+sudo python3 montana.py argv[1] argv[2] ... argv[5]
+```
+###  Arguments 
+
+* argv[1] : Logical name of the interface you want to sniff on (wlanX, ethX, ....)  
+
+* argv[2]: Length of the sniffing session in seconds.  
+
+* argv[3]: "-nstat" displays the statistics of the sniffing session  
+
+* argv[4]: "-list" list the content of the captured packets  
+
+* argv[5]: specification of -list option  
+        --udp = lists udp packets  
+        --tcp = lists tcp packets  
+        --arp = lists arp packets  
+        --icmp = lists icmp packets  
+        --all = lists all the protocols implemented  
+        --no = doesn't list any kind of packets  
+
+Example :
+```bash
+#Listen on wlan0 during 30 seconds, displays the statistical results and list icmp packets captured 
+
+sudo python3 montana.py wlan0 30 -nstat -list --icmp
+```
+
+
--- montana.py
+++ montana.py
@@ -10,19 +10,26 @@ import numpy as np
 
 """ Home Sniffer Montana v1.0
 
-Call script : sudo python3 montana.py INTERFACE timetocapture
+Call script : sudo python3 montana.py INTERFACE timetocapture [-nstat] -list --PROTOCOL
   
 OSI LAYERS
 
 (7) [APPLICATIONS] |HTTP/FTP/SSH/DNS|
 (6) [PRESENTATION] |SSL/SSH/IMAP/FTP|
 (5) [SESSION     ] |APIs / SOCKET   |
-(4) [TRANSPORT   ] |TCP     /    UDP|
-(3) [RESEAU      ] |IP / ICMP/ IPSec|
-(2) [LIEN        ] |Ethernet / PPP  |
-(1) [PHYSIQUE    ] |###UNUSED HERE##|
+(4) [TRANSPORT   ] |TCP     /    UDP| ---> Operate
+(3) [NETWORK     ] |IP / ICMP/ IPSec| ---> Operate
+(2) [LINK        ] |Ethernet / PPP  |
+(1) [PHYSICAL    ] |###UNUSED HERE##|
 
 """
+
+print(r""" __  __  ____  _   _ _______       _   _          
+    |  \/  |/ __ \| \ | |__   __|/\   | \ | |   /\    
+    | \  / | |  | |  \| |  | |  /  \  |  \| |  /  \   
+    | |\/| | |  | | . ` |  | | / /\ \ | . ` | / /\ \  
+    | |  | | |__| | |\  |  | |/ ____ \| |\  |/ ____ \ 
+    |_|  |_|\____/|_| \_|  |_/_/    \_\_| \_/_/    \_\ \r\n""")
 
 interface = argv[1] ## ARGV[0] = script title 
 tOut = int(argv[2])
@@ -54,42 +61,53 @@ def pie_Plot(udp=udp,tcp=tcp,icmp=icmp,arp=arp):
     plt.pie(sizes,labels=labels)
     plt.plot()
 
-def list_Packets(protocol=None):
-    if len(protocol) == 0:
-        exit(ValueError)
-    for i in range(len(protocol)):
-        print("*** PACKET N° " + str(i) + "\n\r" )
-        protocol[i].show()
-
-
-print("*** Welcome to Montana *** \n\r")
-selector=input("Do you want to sniff over interface "+argv[1]+" during "+argv[2]+" seconds ? [y/N]")
-
-if selector=='y' or selector=='Y':
-    print("Let's sniff !!!!\r\n")
-    sniff(filter=filters[0],count=0,prn=lambda x:store_Packet(x),iface=interface,timeout=tOut)
+def display_Net_Stats(udp=None,tcp=None,arp=None,icmp=None):
     fullScale=total_Packets(udp,tcp,icmp,arp)
-    # Tests & Debug
-    print("Total sniffed packets ="+str(fullScale)+"\n\r")
+    print("Total sniffed packets ="+str(fullScale)+"\r\n")
     print(str(100*compute_Percentage(udp,fullScale))+"% \of UDP \r\n")
     print(str(100*compute_Percentage(tcp,fullScale))+"% \of TCP \r\n")
     print(str(100*compute_Percentage(icmp,fullScale))+"% \of ICMP \r\n")
     print(str(100*compute_Percentage(arp,fullScale))+"% \of ARP \r\n")
 
-    if argv[3]=="-list":
-        if argv[4] =="--udp":
+def list_Packets(protocol=None):
+    if len(protocol) == 0:
+        exit(ValueError)
+    for i in range(len(protocol)):
+        print("*** PACKET N° " + str(i) + "\r\n" )
+        protocol[i].show()
+
+
+print("*** Welcome to Montana *** \r\n")
+selector=input("Do you want to sniff over interface "+argv[1]+" during "+argv[2]+" seconds ? [y/N]")
+
+if selector=='y' or selector=='Y':
+    print("Let's sniff !!!!\r\n")
+    sniff(filter=filters[0],count=0,prn=lambda x:store_Packet(x),iface=interface,timeout=tOut)
+
+    if argv[3]=="-nstat":
+        display_Net_Stats(udp,tcp,arp,icmp)
+    else:
+        print("** No stats \r\n")
+
+    if argv[4]=="-list":
+        if argv[5] =="--udp":
             list_Packets(udp)
-        if argv[4] =="--tcp":
+        if argv[5] =="--tcp":
             list_Packets(tcp)
-        if argv[4] =="--arp":
+        if argv[5] =="--arp":
             list_Packets(arp)
-        if argv[4] =="--icmp":
+        if argv[5] =="--icmp":
             list_Packets(icmp)
-        if argv[4] =="--all":
+        if argv[5] =="--all":
             list_Packets(udp)
             list_Packets(tcp)
             list_Packets(arp)
             list_Packets(icmp)
+        if argv[5] == "--no":
+            print("No list \r\n")
+
 else:
-    print("Thanks for using ! Goodbye ! \n\r")
+    print("Wtf bro ?\r\n")
+
+print("Thanks for using ! Goodbye ! \r\n")
 
